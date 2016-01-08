306fed981298dd0467d9b92250c072d939f34df4
Niko Salakka <nsalakka@gmail.com>
2016-01-08 22:47:02 +0200

    coolio

--- 1.js
+++ 1.js
@@ -1,14 +1,28 @@
-//init stuff
-var scene = new THREE.Scene();
-var renderer = new THREE.WebGLRenderer();
-renderer.setClearColor(0x000011);
+var scene, renderer, camera;
+var cube;
 
-var camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.set(0, 5, 50);
-camera.lookAt(scene.position);
+function init(){
+    //init stuff
+    scene = new THREE.Scene();
+    renderer = new THREE.WebGLRenderer();
+    camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
 
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
+    //configure things
+    renderer.setClearColor(0x000011);
+    renderer.setSize(window.innerWidth, window.innerHeight);
+
+    camera.position.set(0, 5, 50);
+    camera.lookAt(scene.position);
+
+    document.body.appendChild(renderer.domElement);
+    window.addEventListener('resize', resizeWindow, false);
+}
+
+function resizeWindow(){
+    camera.aspect = window.innerWidth / window.innerHeight; 
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+}
 
 var shader = new THREE.ShaderMaterial({
     uniforms: THREE.UniformsUtils.merge([
@@ -25,42 +39,48 @@ THREE.PointLight.prototype.addSphere = function(){
     this.add(this.sphere);
 };
 
+
 //create scene
-var ambientLight = new THREE.AmbientLight(0x111111);
-scene.add(ambientLight);
+function sceneSetup(){
 
-var pointLight = new THREE.PointLight(0xFF0000,1,100);
-pointLight.addSphere();
-pointLight.position.set(10, 0, 0);
-scene.add(pointLight);
+    var ambientLight = new THREE.AmbientLight(0x111111);
+    scene.add(ambientLight);
 
-var pointLight2 = new THREE.PointLight(0x00FF00,1,100);
-pointLight2.addSphere();
-pointLight2.position.set(0, 10, 0);
-scene.add(pointLight2);
+    var pointLight = new THREE.PointLight(0xFF0000,1,100);
+    pointLight.addSphere();
+    pointLight.position.set(10, 0, 0);
+    scene.add(pointLight);
 
-var pointLight3 = new THREE.PointLight(0x0000FF,1,100);
-pointLight3.addSphere();
-pointLight3.position.set(0, 0, 10);
-scene.add(pointLight3);
+    var pointLight2 = new THREE.PointLight(0x00FF00,1,100);
+    pointLight2.addSphere();
+    pointLight2.position.set(0, 10, 0);
+    scene.add(pointLight2);
 
-var pointLight4 = new THREE.PointLight(0xFFFFFF,1,100);
-pointLight4.addSphere();
-pointLight4.position.set(-8, 0, -8);
-scene.add(pointLight4);
+    var pointLight3 = new THREE.PointLight(0x0000FF,1,100);
+    pointLight3.addSphere();
+    pointLight3.position.set(0, 0, 10);
+    scene.add(pointLight3);
 
-shader.needsUpdate = true;
+    var pointLight4 = new THREE.PointLight(0xFFFFFF,1,100);
+    pointLight4.addSphere();
+    pointLight4.position.set(-8, 0, -8);
+    scene.add(pointLight4);
 
-var cube = new THREE.Mesh(new THREE.BoxGeometry(5, 5, 5),
-                          shader);
-cube.position.set(0, 0, 0);
-scene.add(cube);
+    shader.needsUpdate = true;
 
-var ground = new THREE.Mesh(new THREE.PlaneGeometry(100, 100),
+    cube = new THREE.Mesh(new THREE.BoxGeometry(5, 5, 5),
                             shader);
-ground.position.set(0, -5, 0);
-ground.rotation.x = -90 * (Math.PI / 180);
-scene.add(ground);
+
+    cube.position.set(0, 0, 0);
+    scene.add(cube);
+
+    var ground = new THREE.Mesh(new THREE.PlaneGeometry(100, 100),
+                                shader);
+    ground.position.set(0, -5, 0);
+    ground.rotation.x = -90 * (Math.PI / 180);
+    scene.add(ground);
+
+}
 
 
 function render() {
@@ -77,4 +97,7 @@ function render() {
 
 	renderer.render(scene, camera);
 }
+
+init();
+sceneSetup();
 render();
